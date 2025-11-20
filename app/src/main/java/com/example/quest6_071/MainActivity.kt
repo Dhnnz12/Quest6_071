8182ee9d9398c812de35dd4a52b3fd77bb2fedc0
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-20 12:15:08 +0700

    memanggil Data App di mainactivity

--- app/src/main/java/com/example/quest6_071/MainActivity.kt
+++ app/src/main/java/com/example/quest6_071/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quest6_071.ui.theme.Quest6_071Theme
+import com.example.quest6_071.view.uicontroller.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Quest6_071Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Quest6_071Theme {
-        Greeting("Android")
     }
 }
