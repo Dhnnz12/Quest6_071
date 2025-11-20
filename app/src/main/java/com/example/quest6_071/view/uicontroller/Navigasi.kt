f1314643494a32511a6e10e5ab6fc82fb84730fa
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-20 13:39:21 +0700

    memperbarui Navigasi untuk menangani status dan logika formulir

--- app/src/main/java/com/example/quest6_071/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/quest6_071/view/uicontroller/Navigasi.kt
@@ -3,12 +3,15 @@ package com.example.quest6_071.view.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.quest6_071.model.DataJK.JenisK
 import com.example.quest6_071.view.FormIsian
 import com.example.quest6_071.view.TampilData
 import com.example.quest6_071.viewmodel.SiswaViewModel
@@ -28,15 +31,22 @@ fun DataApp(
    navController : NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang->
+
+        val uiState = viewModel.statusUI.collectAsState()
+
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier.padding(isiRuang)) {
             composable(route = Navigasi.Formulirku.name) {
+                val konteks = LocalContext.current
                 FormIsian(
+                    pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
                     OnSubmitBtnClick = {
+                        viewModel.setSiswa(it)
                         navController.navigate(Navigasi.Detail.name)
+
                     }
                 )
             }
