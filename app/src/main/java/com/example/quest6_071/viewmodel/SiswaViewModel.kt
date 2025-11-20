9ef7b941a4ad093bf90e33e39a833d97acbf6c02
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-20 13:04:34 +0700

    perbaiki code

--- app/src/main/java/com/example/quest6_071/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_071/viewmodel/SiswaViewModel.kt
@@ -5,10 +5,9 @@ import com.example.quest6_071.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel: ViewModel() {
-
     private val _statusUI = MutableStateFlow(Siswa())
 
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
